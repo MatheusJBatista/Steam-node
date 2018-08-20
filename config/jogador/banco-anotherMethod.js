45da987ea575fd9dae49d480f096f85a70152d59
Matheus Roberto J. Batista <matheus_roberto_batista@hotmail.com>
2018-08-20 16:18:06 -0300

    bdConfig

--- config/jogador/banco-anotherMethod.js
+++ config/jogador/banco-anotherMethod.js
@@ -1,4 +1,4 @@
-const banco = 'postgres://ruptjkilyotmae:a431ea3c7079749c8effe5e14953bc6107d35a6b5fb0885b49014fcb94b4afb0@ec2-50-17-194-129.compute-1.amazonaws.com:5432/d2o9bmebg4cijo';
+const banco = <key>
 const {Pool} = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL || banco,
