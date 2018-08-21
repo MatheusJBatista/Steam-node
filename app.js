84df3a4146335b355c72efeffd2caad23eea60c7
Matheus Roberto J. Batista <matheus_roberto_batista@hotmail.com>
2018-08-21 13:41:51 -0300

    bugFix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('./config/server');
 var home = require('./app/routes/steam/home')(app);
 var home = require('./app/routes/steam/auth')(app);
 var home = require('./app/routes/steam/verify')(app);
-var app = process_env_PORT || 5000
+var port = process.env.PORT || 5000
 
 
 app.listen(port, function(){
--- config/server.js
+++ config/server.js
@@ -9,7 +9,7 @@ app.use(require('express-session')({ resave: false, saveUninitialized: false, se
 app.use(steam.middleware({
     realm: 'http://localhost:5000/',
     verify: 'http://localhost:5000/verify',
-    apiKey: <key>}
+    apiKey: '69FC736459FCC5094E6CE76DCD0A466D'}
 ));
 
 app.use(express.static('./app/public'));
