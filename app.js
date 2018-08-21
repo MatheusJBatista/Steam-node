b27ce2e56eeb2dc497c94842ff7e9e0df0f82479
Matheus Roberto J. Batista <matheus_roberto_batista@hotmail.com>
2018-08-21 13:35:33 -0300

    bugFix

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var app = require('./config/server');
 var home = require('./app/routes/steam/home')(app);
 var home = require('./app/routes/steam/auth')(app);
 var home = require('./app/routes/steam/verify')(app);
+var app = process_env_PORT || 5000
 
 
-app.listen(3000, function(){
-  console.log('Servidor Rodando na porta 3000');
+app.listen(port, function(){
+  console.log('Servidor Rodando na porta 5000');
 });
